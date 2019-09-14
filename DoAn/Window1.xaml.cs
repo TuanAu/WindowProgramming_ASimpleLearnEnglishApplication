49b675598b5e8e32d61465b6471f7c589cd29fc3
tuan <adgtuan@gmail.com>
2019-09-14 18:53:21 +0700

    2

--- DoAn/Window1.xaml.cs
+++ DoAn/Window1.xaml.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.IO;
+using System.Timers;
+using System.Linq;
+using System.Windows.Media.Imaging;
 using System.Diagnostics;
 
 namespace DoAn
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
+   
     public partial class Window1 : Window
     {
+       Timer timer;
         public Window1()
         {
             InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 3000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
         }
 
 
@@ -43,7 +39,6 @@ namespace DoAn
         {
             //Read images file
             string path = Directory.GetCurrentDirectory();
-            Debug.Write(path);
             string[] lines = File.ReadAllLines(path + "\\data.txt");
 
             //token
@@ -59,6 +54,38 @@ namespace DoAn
             }
 
             MessageBox.Show("Are you ready?");
+        }
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            const string file = ".jpg";
+            int Max = image.Count;
+            Random rdn = new Random();
+            int index = rdn.Next(Max);
+            string fileName = image[index] + file;
+            Debug.WriteLine(fileName);
+            Dispatcher.Invoke(() =>
+            {
+                var pic = new BitmapImage(new Uri("images/" + fileName, UriKind.Relative));
+                Image.Source = pic;
+                VocabLabel.Content = $"{vocabulary[index]}";
+            }
+            );
+        }
+
+        private void TimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (timer.Enabled == true)
+            {
+                TimerButton.Content = "Start";
+                timer.Enabled = false;
+                timer.Stop();
+            }
+            else
+            {
+                TimerButton.Content = "Stop";
+                timer.Enabled = true;
+                timer.Start();
+            }
         }
     }
 }
